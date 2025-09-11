2efa9cca4cce32c254dcedb85445edda4054857d
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-09-10 23:59:40 -0500

    Update CitaDTO.java

--- src/main/java/com/upc/vitalco/dto/CitaDTO.java
+++ src/main/java/com/upc/vitalco/dto/CitaDTO.java
@@ -1,10 +1,6 @@
 package com.upc.vitalco.dto;
 
-import com.upc.vitalco.entidades.Nutricionista;
-import com.upc.vitalco.entidades.Paciente;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -12,15 +8,13 @@ import java.time.LocalTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class CitaDTO {
     private Integer id;
-    private Paciente idpaciente;
-    private Nutricionista idnutricionista;
     private LocalDate dia;
     private LocalTime hora;
     private String descripcion;
     private String estado;
     private String link;
+    private Integer idPaciente;
+    private Integer idNutricionista;
 }
