7a5a19304f49cac58c6b72da9adec16d030d83f0
Melanie Oliva <mel.olivac@gmail.com>
2025-09-10 22:36:16 -0500

    Agregando mantenimiento a la tabla Paciente

---
+++ src/main/java/com/upc/vitalco/controllers/PacienteController.java
@@ -0,0 +1,36 @@
+package com.upc.vitalco.controllers;
+
+import com.upc.vitalco.dto.PacienteDTO;
+import com.upc.vitalco.services.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class PacienteController {
+    @Autowired
+    private PacienteService pacienteService;
+
+    @PostMapping("/registrarPaciente")
+    public PacienteDTO registrar(@RequestBody PacienteDTO dto) {
+        return pacienteService.registrar(dto);
+    }
+
+    @GetMapping("/listarPacientes")
+    public List<PacienteDTO> findAll(){
+        return pacienteService.findAll();
+    }
+
+    @DeleteMapping("/eliminarPaciente/{id}")
+    public void eliminar(@PathVariable Integer id){
+        pacienteService.eliminar(id);
+    }
+
+    @PutMapping("/editarPaciente")
+    public ResponseEntity<PacienteDTO> editar(@RequestBody PacienteDTO dto) {
+        return ResponseEntity.ok(pacienteService.actualizar(dto));
+    }
+}
---
+++ src/main/java/com/upc/vitalco/dto/PacienteDTO.java
@@ -0,0 +1,22 @@
+package com.upc.vitalco.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PacienteDTO {
+    private Integer id;
+    private UsuarioDTO idusuario;
+    private BigDecimal altura;
+    private BigDecimal peso;
+    private Integer edad;
+    private PlanSuscripcionDTO idplan;
+    private String actividadFisica;
+    private BigDecimal trigliceridos;
+}
