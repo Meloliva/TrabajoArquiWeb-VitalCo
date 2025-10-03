4412964f1b6d8e8a4f46c6996219273849e08815
luz21-brownie <luzclaritacochachi21@gmail.com>
2025-10-03 15:57:29 -0500

    excepciones de duplicidad de dni

--- src/main/java/com/upc/vitalco/excepciones/GlobalExceptionHandler.java
+++ src/main/java/com/upc/vitalco/excepciones/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.upc.vitalco.excepciones;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Collections;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "El DNI proporcionado ya existe en la base de datos. Por favor, utilice un DNI único.",
+                Collections.singletonList(ex.getMessage())
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
