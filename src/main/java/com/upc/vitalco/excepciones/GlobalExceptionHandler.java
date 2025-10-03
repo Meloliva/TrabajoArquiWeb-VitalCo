73b2b4c05a1dc7763ea8db2d3ef4e9d0a4ffad3a
luz21-brownie <luzclaritacochachi21@gmail.com>
2025-10-03 16:01:19 -0500

    excepcion de que esta mal el formato Json

--- src/main/java/com/upc/vitalco/excepciones/GlobalExceptionHandler.java
+++ src/main/java/com/upc/vitalco/excepciones/GlobalExceptionHandler.java
@@ -3,10 +3,12 @@ package com.upc.vitalco.excepciones;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Collections;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,6 +19,15 @@ public class GlobalExceptionHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 "El DNI proporcionado ya existe en la base de datos. Por favor, utilice un DNI único.",
                 Collections.singletonList(ex.getMessage())
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleJsonParseError(HttpMessageNotReadableException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Error en el formato del JSON: Datos no válidos.",
+                List.of("Se ha producido un error al analizar el JSON. Verifica que todos los campos estén correctamente definidos y que no haya valores vacíos o comas extra.")
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
