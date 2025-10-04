ee6de670221152f4b92fa279185dd5456040d14f
Maripareja <mari.pavi.06@gmail.com>
2025-10-04 18:46:03 -0500

    Quitando funciones vacias en plan receta receta

--- src/main/java/com/upc/vitalco/repositorios/PlanRecetaRecetaRepositorio.java
+++ src/main/java/com/upc/vitalco/repositorios/PlanRecetaRecetaRepositorio.java
@@ -2,21 +2,13 @@ package com.upc.vitalco.repositorios;
 
 import com.upc.vitalco.entidades.PlanRecetaReceta;
 import com.upc.vitalco.entidades.Planreceta;
-import com.upc.vitalco.entidades.Receta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PlanRecetaRecetaRepositorio extends JpaRepository<PlanRecetaReceta, Long >{
-    Optional<PlanRecetaReceta> findByPlanrecetaIdAndRecetaId(Integer planrecetaId, Long recetaId);
-    Optional<PlanRecetaReceta> findByPlanrecetaAndReceta(Planreceta planreceta, Receta receta);
-
-    List<PlanRecetaReceta> findPlanRecetaRecetaByPlanreceta(Planreceta planreceta);
     List<PlanRecetaReceta> findByPlanreceta(Planreceta planreceta);
-
-    List<PlanRecetaReceta> findByPlanrecetaId(Integer planrecetaId);
 }
 
