98e27fe1029bded4107e1bdf4e2b61e683bda61f
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-10-04 23:36:47 -0500

    Update PacienteController.java

--- src/main/java/com/upc/vitalco/controllers/PacienteController.java
+++ src/main/java/com/upc/vitalco/controllers/PacienteController.java
@@ -4,6 +4,7 @@ import com.upc.vitalco.dto.PacienteDTO;
 import com.upc.vitalco.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,16 +21,19 @@ public class PacienteController {
     }
 
     @GetMapping("/listarPacientes")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<PacienteDTO> findAll(){
         return pacienteService.findAll();
     }
 
     @DeleteMapping("/eliminarPaciente/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void eliminar(@PathVariable Integer id){
         pacienteService.eliminar(id);
     }
 
     @PutMapping("/editarPaciente")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PacienteDTO> editar(@RequestBody PacienteDTO dto) {
         return ResponseEntity.ok(pacienteService.actualizar(dto));
     }
