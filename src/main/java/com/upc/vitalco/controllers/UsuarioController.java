1a41b93436c29e0ee005c8f5a7a22ce80e363489
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-10-04 23:12:07 -0500

    Update UsuarioController.java

--- src/main/java/com/upc/vitalco/controllers/UsuarioController.java
+++ src/main/java/com/upc/vitalco/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import com.upc.vitalco.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 
 import java.util.List;
 
