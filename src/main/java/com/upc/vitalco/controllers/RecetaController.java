8e93de39af1e2739bfb0e209d2195a3a39947142
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-10-04 23:39:06 -0500

    Update RecetaController.java

--- src/main/java/com/upc/vitalco/controllers/RecetaController.java
+++ src/main/java/com/upc/vitalco/controllers/RecetaController.java
@@ -5,6 +5,7 @@ import com.upc.vitalco.services.RecetaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class RecetaController {
     @Autowired
     private RecetaService recetaService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/registrarReceta")
     public RecetaDTO registrar(@RequestBody RecetaDTO recetaDTO){ //wrapper
         return recetaService.registrar(recetaDTO);
@@ -22,25 +24,27 @@ public class RecetaController {
     public RecetaDTO registrarRecetaNutri(@RequestBody RecetaDTO recetaDTO) {
         return recetaService.registrarNutri(recetaDTO);
     }*/
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/listarRecetas")
     public List<RecetaDTO> findAll(){
         return recetaService.findAll();
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/eliminarReceta/{id}")
     public void eliminar(@PathVariable Long id){
         recetaService.eliminarReceta(id);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/editarReceta")
     public ResponseEntity<RecetaDTO> editarReceta(@RequestBody RecetaDTO recetaDTO){
         return ResponseEntity.ok(recetaService.actualizar(recetaDTO));
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/buscarRecetasPorNombre")
     public List<RecetaDTO> buscarPorNombre(@RequestParam String nombre) {
         return recetaService.buscarRecetasPorNombre(nombre);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/autocompletarNombresRecetas")
     public List<String> autocompletarNombres(@RequestParam String texto) {
         return recetaService.autocompletarNombresRecetas(texto);
