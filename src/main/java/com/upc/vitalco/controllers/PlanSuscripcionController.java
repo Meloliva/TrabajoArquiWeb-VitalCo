1d27c5dcf18fcdcfce39de49f45d7576e508cd66
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-10-04 23:38:32 -0500

    Update PlanSuscripcionController.java

--- src/main/java/com/upc/vitalco/controllers/PlanSuscripcionController.java
+++ src/main/java/com/upc/vitalco/controllers/PlanSuscripcionController.java
@@ -5,6 +5,7 @@ import com.upc.vitalco.services.PlanSuscripcionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -14,21 +15,25 @@ public class PlanSuscripcionController {
     @Autowired
     private PlanSuscripcionService planSuscripcionService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/registrarPlanSuscripcion")
     public PlanSuscripcionDTO registrar(@RequestBody PlanSuscripcionDTO planSuscripcionDTO){
         return planSuscripcionService.registrar(planSuscripcionDTO);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/listarPlanesSuscripcion")
     public List<PlanSuscripcionDTO> findAll(){
         return planSuscripcionService.findAll();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/eliminarPlanSuscripcion/{id}")
     public void eliminar(@PathVariable Integer id){
         planSuscripcionService.eliminarReceta(id);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/editarPlanSuscripcion")
     public ResponseEntity<PlanSuscripcionDTO> editar(@RequestBody PlanSuscripcionDTO planSuscripcionDTO){
         return ResponseEntity.ok(planSuscripcionService.actualizar(planSuscripcionDTO));
