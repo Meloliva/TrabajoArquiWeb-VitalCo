8842d0d9e047b25a0cbc13ef5dbba3add7732211
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-10-04 23:36:28 -0500

    Update HorarioController.java

--- src/main/java/com/upc/vitalco/controllers/HorarioController.java
+++ src/main/java/com/upc/vitalco/controllers/HorarioController.java
@@ -4,6 +4,7 @@ import com.upc.vitalco.dto.HorarioDTO;
 import com.upc.vitalco.services.HorarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,21 +15,26 @@ public class HorarioController {
     @Autowired
     private HorarioService horarioService;
 
-
     @PostMapping("/registraHorario")
+    @PreAuthorize("hasRole('ADMIN')")
     public HorarioDTO registrar(@RequestBody HorarioDTO horarioDTO){
         return horarioService.registrar(horarioDTO);
     }
+
     @PutMapping("/actualizarHorario")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<HorarioDTO> actualizar(@RequestBody HorarioDTO horarioDTO){
         return ResponseEntity.ok(horarioService.actualizar(horarioDTO));
     }
+
     @GetMapping("/listarHorarios")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<HorarioDTO> findAll(){
         return  horarioService.findAll();
     }
 
     @DeleteMapping("/eliminarHorario/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void borrar(@PathVariable Long id){
         horarioService.borrar(id);
     }
