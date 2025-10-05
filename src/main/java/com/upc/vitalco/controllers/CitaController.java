39a14da57272a1ded65b0aeef6d59618d8e9cb07
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-10-04 23:12:34 -0500

    Update CitaController.java

--- src/main/java/com/upc/vitalco/controllers/CitaController.java
+++ src/main/java/com/upc/vitalco/controllers/CitaController.java
@@ -4,6 +4,7 @@ import com.upc.vitalco.dto.CitaDTO;
 import com.upc.vitalco.services.CitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
