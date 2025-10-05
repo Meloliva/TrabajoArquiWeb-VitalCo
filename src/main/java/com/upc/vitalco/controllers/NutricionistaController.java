e4d67d89d7890d505076a0ff6d51f3fe873e8a29
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-10-04 23:36:39 -0500

    Update NutricionistaController.java

--- src/main/java/com/upc/vitalco/controllers/NutricionistaController.java
+++ src/main/java/com/upc/vitalco/controllers/NutricionistaController.java
@@ -4,6 +4,7 @@ import com.upc.vitalco.dto.NutricionistaDTO;
 import com.upc.vitalco.services.NutricionistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,16 +21,19 @@ public class NutricionistaController {
     }
 
     @GetMapping("/listarNutricionistas")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<NutricionistaDTO> findAll(){
         return nutricionistaService.findAll();
     }
 
     @DeleteMapping("/eliminarNutricionista/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void eliminar(@PathVariable Integer id){
         nutricionistaService.eliminar(id);
     }
 
     @PutMapping("/editarNutricionista")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<NutricionistaDTO> editar(@RequestBody NutricionistaDTO dto) {
         return ResponseEntity.ok(nutricionistaService.actualizar(dto));
     }
