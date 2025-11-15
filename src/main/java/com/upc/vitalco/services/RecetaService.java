e0df4e859484958fb590065ec5d4a0d2c1acc7cd
KokyRamirezPerez <koky.ramirez26@outlook.com>
2025-11-15 13:47:02 -0500

    actualizando el editar receta

--- src/main/java/com/upc/vitalco/services/RecetaService.java
+++ src/main/java/com/upc/vitalco/services/RecetaService.java
@@ -1,13 +1,11 @@
 package com.upc.vitalco.services;
 
 import com.upc.vitalco.dto.RecetaDTO;
-import com.upc.vitalco.entidades.Paciente;
 import com.upc.vitalco.entidades.Receta;
-import com.upc.vitalco.entidades.Seguimiento;
+import com.upc.vitalco.entidades.Horario;
 import com.upc.vitalco.interfaces.IRecetaServices;
-import com.upc.vitalco.repositorios.PacienteRepositorio;
+import com.upc.vitalco.repositorios.HorarioRepositorio;
 import com.upc.vitalco.repositorios.RecetaRepositorio;
-import com.upc.vitalco.repositorios.SeguimientoRepositorio;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +18,8 @@ import java.util.stream.Collectors;
 public class RecetaService implements IRecetaServices {
     @Autowired
     private RecetaRepositorio recetaRepositorio;
+    @Autowired
+    private HorarioRepositorio horarioRepositorio;
     @Autowired
     private ModelMapper modelMapper;
 
@@ -53,6 +53,13 @@ public class RecetaService implements IRecetaServices {
         Receta recetaExistente = recetaRepositorio.findById(recetaDTO.getIdReceta())
                 .orElseThrow(() -> new RuntimeException("Receta con ID " + recetaDTO.getIdReceta() + " no encontrado"));
 
+        // Actualizar horario
+        if (recetaDTO.getIdhorario() != null) {
+            Horario horario = horarioRepositorio.findById(recetaDTO.getIdhorario().getId())
+                    .orElseThrow(() -> new RuntimeException("Horario no encontrado"));
+            recetaExistente.setIdhorario(horario);
+        }
+
         recetaExistente.setNombre(recetaDTO.getNombre());
         recetaExistente.setDescripcion(recetaDTO.getDescripcion());
         recetaExistente.setTiempo(recetaDTO.getTiempo());
@@ -61,8 +68,17 @@ public class RecetaService implements IRecetaServices {
         recetaExistente.setGrasas(recetaDTO.getGrasas());
         recetaExistente.setProteinas(recetaDTO.getProteinas());
 
+        // ❗ Estos estaban faltando
+        recetaExistente.setIngredientes(recetaDTO.getIngredientes());
+        recetaExistente.setPreparacion(recetaDTO.getPreparacion());
+        recetaExistente.setCantidadPorcion(recetaDTO.getCantidadPorcion());
+        if (recetaDTO.getFoto() != null && !recetaDTO.getFoto().isEmpty()) {
+            recetaExistente.setFoto(recetaDTO.getFoto());
+        }
+
         Receta guardado = recetaRepositorio.save(recetaExistente);
         return modelMapper.map(guardado, RecetaDTO.class);
     }
+
 
 }
